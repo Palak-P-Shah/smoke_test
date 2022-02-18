fa0d2a4dd8ce067626bfc88d2a603325d3469954
Palak P Shah <dabbadolcha@gmail.com>
2022-02-19 01:34:57 +0530

    browserstack jenkins test 6

--- ShadowAndAct/Android/scroll_carousel_android_chrome_shadowandact.py
+++ ShadowAndAct/Android/scroll_carousel_android_chrome_shadowandact.py
@@ -26,7 +26,7 @@ desired_cap = {
     'os': 'Android',
     'name': 'BStack-[Python] Smoke Test for staging.shadowandact.com in '
            'carousel for left and right slides',  # test name
-    'build': build_name,
+    'build': "BROWSERSTACK_BUILD_NAME",
     # 'browserstack.local': browserstack_local,
     # 'browserstack.localIdentifier': browserstack_local_identifier,
     # 'browserstack.user': username,
